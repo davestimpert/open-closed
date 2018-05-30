6c61b0a43416af7ec5b674b8eb9e7321602bff16
Dave Stimpert <dave.stimpert@robotsandpencils.com>
2018-05-30 15:51:14 -0400

    Add inventory tracking

--- src/com/example/ShoppingCart.java
+++ src/com/example/ShoppingCart.java
@@ -6,12 +6,15 @@ import java.util.UUID;
 public class ShoppingCart {
 
     private final CartRepository cartRepository;
+    private final Inventory inventory;
 
-    public ShoppingCart(CartRepository cartRepository) {
+    public ShoppingCart(CartRepository cartRepository, Inventory inventory) {
         this.cartRepository = cartRepository;
+        this.inventory = inventory;
     }
 
     public UUID addItem(Item item, User user) {
+        inventory.decrement(item.getSku(), item.getQty());
         return cartRepository.addItem(item, user);
     }
 
