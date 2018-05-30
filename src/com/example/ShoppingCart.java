25b7f2677d9168b59cb18863fe69340a915706af
Dave Stimpert <dave.stimpert@robotsandpencils.com>
2018-05-30 16:00:21 -0400

    Check for promotions

--- src/com/example/ShoppingCart.java
+++ src/com/example/ShoppingCart.java
@@ -1,21 +1,28 @@
 package com.example;
 
+import java.util.List;
 import java.util.UUID;
 
 
-public class ShoppingCart {
+public abstract class ShoppingCart {
 
     private final CartRepository cartRepository;
     private final Inventory inventory;
+    private final Promotions promotions;
 
-    public ShoppingCart(CartRepository cartRepository, Inventory inventory) {
+    public ShoppingCart(CartRepository cartRepository, Inventory inventory, Promotions promotions) {
         this.cartRepository = cartRepository;
         this.inventory = inventory;
+        this.promotions = promotions;
     }
 
     public UUID addItem(Item item, User user) {
         inventory.decrement(item.getSku(), item.getQty());
-        return cartRepository.addItem(item, user);
+        UUID uuid = cartRepository.addItem(item, user);
+        promotions.checkForPromotions(getItems());
+        return uuid;
     }
+
+    abstract List<Long> getItems();
 
 }
